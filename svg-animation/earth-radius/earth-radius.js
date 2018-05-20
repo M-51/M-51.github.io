e16d6f2cf11f7cf4862bc495cb8bdd211cce995f
M51 <m51@m51.pl>
2018-05-20 20:09:40 +0200

    update examples

--- svg-animation/earth-radius/earth-radius.js
+++ svg-animation/earth-radius/earth-radius.js
@@ -57,6 +57,19 @@ earthRadius.add({
                     to: 300,
                 },
             },
+        }, {
+            range: [76, 77],
+            scale: {
+                to: 0.5,
+            },
+            translate: {
+                x: {
+                    to: 250,
+                },
+                y: {
+                    to: 250,
+                },
+            },
         }],
     },
 }, {
@@ -252,12 +265,17 @@ earthRadius.add({
                 to: 0,
             },
         },
-        x2: {
+        x2: [{
             range: [53, 54],
             value: {
                 to: 500,
             },
-        },
+        }, {
+            range: [66, 67],
+            value: {
+                to: 400,
+            },
+        }],
     },
 }, {
     object: '#alexandria-dot',
@@ -291,12 +309,17 @@ earthRadius.add({
 }, {
     object: '#alexandria-stick',
     animation: {
-        x2: {
+        x2: [{
             range: [32, 33],
             value: {
                 to: 500,
             },
-        },
+        }, {
+            range: [66, 67],
+            value: {
+                to: 400,
+            },
+        }],
         x1: {
             range: [53, 54],
             value: {
@@ -375,6 +398,16 @@ earthRadius.add({
             },
         },
     },
+}, {
+    object: '#distance',
+    animation: {
+        opacity: {
+            range: [70, 71],
+            value: {
+                to: 1,
+            },
+        },
+    },
 });
 
 /* sun rays */
@@ -408,6 +441,10 @@ const otherRays = document.getElementsByClassName('other-rays');
 earthRadius.add({
     object: '#text-1',
     animation: {
+        display: {
+            range: 3,
+            value: 'none',
+        },
         opacity: {
             range: [2, 3],
             value: {
@@ -419,6 +456,13 @@ earthRadius.add({
 }, {
     object: '#text-2',
     animation: {
+        display: [{
+            range: 3,
+            value: 'block',
+        }, {
+            range: 25,
+            value: 'none',
+        }],
         opacity: [{
             range: [3, 4],
             value: {
@@ -434,6 +478,13 @@ earthRadius.add({
 }, {
     object: '#text-3',
     animation: {
+        display: [{
+            range: 10,
+            value: 'block',
+        }, {
+            range: 25,
+            value: 'none',
+        }],
         opacity: [{
             range: [10, 11],
             value: {
@@ -449,6 +500,13 @@ earthRadius.add({
 }, {
     object: '#text-4',
     animation: {
+        display: [{
+            range: 16,
+            value: 'block',
+        }, {
+            range: 25,
+            value: 'none',
+        }],
         opacity: [{
             range: [16, 17],
             value: {
@@ -464,6 +522,13 @@ earthRadius.add({
 }, {
     object: '#text-5',
     animation: {
+        display: [{
+            range: 27,
+            value: 'block',
+        }, {
+            range: 41,
+            value: 'none',
+        }],
         opacity: [{
             range: [27, 28],
             value: {
@@ -479,6 +544,13 @@ earthRadius.add({
 }, {
     object: '#text-6',
     animation: {
+        display: [{
+            range: 32,
+            value: 'block',
+        }, {
+            range: 41,
+            value: 'none',
+        }],
         opacity: [{
             range: [32, 33],
             value: {
@@ -494,6 +566,13 @@ earthRadius.add({
 }, {
     object: '#text-7',
     animation: {
+        display: [{
+            range: 42,
+            value: 'block',
+        }, {
+            range: 51,
+            value: 'none',
+        }],
         opacity: [{
             range: [42, 43],
             value: {
@@ -509,6 +588,13 @@ earthRadius.add({
 }, {
     object: '#text-8',
     animation: {
+        display: [{
+            range: 53,
+            value: 'block',
+        }, {
+            range: 65,
+            value: 'none',
+        }],
         opacity: [{
             range: [53, 54],
             value: {
@@ -524,6 +610,13 @@ earthRadius.add({
 }, {
     object: '#text-9',
     animation: {
+        display: [{
+            range: 56,
+            value: 'block',
+        }, {
+            range: 65,
+            value: 'none',
+        }],
         opacity: [{
             range: [56, 57],
             value: {
@@ -536,4 +629,109 @@ earthRadius.add({
             },
         }],
     },
+}, {
+    object: '#text-10',
+    animation: {
+        display: {
+            range: 66,
+            value: 'block',
+        },
+        opacity: {
+            range: [66, 67],
+            value: {
+                to: 1,
+            },
+        },
+    },
+}, {
+    object: '#text-11',
+    animation: {
+        display: {
+            range: 71,
+            value: 'block',
+        },
+        opacity: {
+            range: [71, 72],
+            value: {
+                to: 1,
+            },
+        },
+    },
+}, {
+    object: '#text-12',
+    animation: {
+        display: {
+            range: 75,
+            value: 'block',
+        },
+        opacity: {
+            range: [75, 76],
+            value: {
+                to: 1,
+            },
+        },
+    },
 });
+
+const group = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+// group.setAttributeNS(null, 'stroke', 'none');
+group.setAttributeNS(null, 'fill', '#f66');
+document.getElementById('earth-group').appendChild(group);
+
+
+for (let i = 1; i < 50; i += 1) {
+    const arc = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    arc.setAttributeNS(null, 'display', 'none');
+    arc.setAttributeNS(null, 'd', 'M0 0 L 400 0 A 400 400 0 0 0 397 -50');
+    arc.setAttributeNS(null, 'opacity', '0.125');
+    arc.setAttributeNS(null, 'transform', `rotate(${-23 - (i * 7.2)})`);
+    group.appendChild(arc);
+
+    earthRadius.add({
+        object: arc,
+        animation: {
+            display: {
+                range: 76 + (i * 0.1),
+                value: 'block',
+            },
+        },
+    });
+}
+
+const distanceText = document.getElementById('distance-text');
+
+earthRadius.add({
+    object: distanceText,
+    animation: {
+        'font-size': [{
+            range: [76, 77],
+            value: {
+                to: 30,
+            },
+        }, {
+            range: 1,
+            value: () => {
+                distanceText.innerHTML = '800 km';
+                return 18;
+            },
+        }],
+    },
+});
+
+earthRadius.add({
+    object: '#distance-line',
+    animation: {
+        d: {
+            range: [76, 81],
+            local: true,
+            value: (t) => {
+                const angle = (-71.5 * t) * (Math.PI / 180);
+                const x = Math.cos(angle) * 420;
+                const y = Math.sin(angle) * 420;
+                distanceText.innerHTML = `${Math.round(800 + (t * 7840))} km`;
+                return `M${x} ${y} A 420 420 0 ${angle > -Math.PI ? 0 : 1} 1 420 0`;
+            },
+        },
+    },
+});
+
